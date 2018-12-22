b1eabd29b33507d06509975fa51bfb253e047059
Tran Quang <trananhquang@Trans-Mac.local>
2018-12-21 23:34:18 -0800

    Initial Commit

---
+++ DATN_PHUC4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DATN_PHUC4
+//
+//  Created by Tran Anh Quang on 12/21/18.
+//  Copyright © 2018 Tran Anh Quang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
